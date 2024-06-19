2813cd088f5d863dfdf347ec29e130f8ae9c86aa
Charles Anako <46936570+Chaan123@users.noreply.github.com>
2024-06-19 06:43:01 -0700

    Assgmnt2Example-CompundInterestCalculator
    First Java Assgmnt CoderCamp

--- src/com/codercamp/javabasics/CharlesAssgmnt2.java
+++ src/com/codercamp/javabasics/CharlesAssgmnt2.java
@@ -1,9 +1,48 @@
 package com.codercamp.javabasics;
 
+import java.util.Scanner;
+
 public class CharlesAssgmnt2 {
 
-	public CharlesAssgmnt2() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		
+		//public CharlesAssgmnt2() {
+	
+		Scanner scanner = new Scanner(System.in);
+		
+		String initialInvestment = userInput(scanner, "Enter Initial Investment");
+		String interestRateInput = userInput(scanner, "Enter Interest Rate");
+		
+		double endOfYearBalance = Integer.parseInt(initialInvestment);
+		double interestRate = Double.parseDouble(interestRateInput);
+		
+		for(int  i = 0; i < 6; i++) {
+			endOfYearBalance = output(endOfYearBalance, interestRate, i);
+		}
+				
+		//System.out.println("Enter Initial Investment");
+		//String initialInvestment = scanner.nextLine();
+		
+		//System.out.println("Enter Interest Rate");
+		//String interestRate = scanner.nextLine();
+		
+		scanner.close();
+	//}
+		
 	}
-
+	
+	private static double output(double endOfYearBalance, double interestRate, int index) {
+		for (int i =0; i < 5; i++); {
+			endOfYearBalance = endOfYearBalance * (interestRate + 1.00);
+		}
+		System.out.println("In " + ((index * 5) + 5) + " years, you should get: " + Math.round(endOfYearBalance));
+		return endOfYearBalance;
+		
+	}
+	private static String userInput(Scanner scanner, String message) {
+		System.out.println(message);
+		String userInput = scanner.nextLine();
+		return userInput;
+	}
+	
 }
